8b4a6c3cb1b1ec8909312b2a2736822cebeeb957
Chitransh-j <135858715+Chitransh-j@users.noreply.github.com>
2023-08-17 16:08:59 +0530

    commit

---
+++ src/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Footer.css'
+
+export const Footer = () => {
+  return (
+    <div className="note"><a href="https://github.com/Chitransh-j">Made By Chitransh Jawere</a></div>
+  )
+}
